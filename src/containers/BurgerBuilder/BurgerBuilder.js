888eef7cd9f0d9a59e2a989beaea5f5210bd0bb5
BrooksPatola <abpatola@myseneca.ca>
2019-09-25 21:42:17 -0400

    adding order summary modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 //import { type } from 'os';
 
 const INGREDIENT_PRICES = {
@@ -82,6 +84,9 @@ removeIngredientHandler = (type) => {
 
         return(
            <Aux>
+               <Modal>
+                   <OrderSummary ingredients={this.state.ingredients}/>
+               </Modal>
               <Burger ingredients={this.state.ingredients} />
                <BuildControls
                ingredientAdded={this.addIngredientHandler}
